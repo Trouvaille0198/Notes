f2201d512713ac5b7ad34161728a27972b732318
MelonSoul <1375911011@qq.com>
2022-08-19 21:15:24 +0800

    update

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+date=date "+%Y-%m-%d %H:%M:%S"
+
+# npm install textlint --global
+# npm install textlint-rule-ja-space-between-half-and-full-width --global
+cd ./content/posts/ || exit
+textlint --fix .
+cd ../..
+git add *
+git commit -m "$date update"
+git push
+ 
